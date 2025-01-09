23fce8dcbb2c74b3a2e834f8dfb194d63664ad5b
behzod dev <111036426+behzod-sharifjonov@users.noreply.github.com>
2025-01-09 15:45:41 +0500

    full finished orders page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
-import Landing from './views/Landing/Landing';
-import AdminLogin from './views/Auth/Login';
-import AdminRegister from './views/Auth/Logout';
-import AdminProfile from './views/Dashboard/Profile/Profile';
-import Dashboard from './views/Dashboard/Dashboard';
-import { createContext, useState } from 'react';
-import { product_data } from './data';
+import React, { createContext, useState, Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import { product_data } from "./data";
+import Loader from "./container/Loader/Loader";
 
-export const  Context = createContext()
+// Lazy loading components
+const Landing = lazy(() => import("./views/Landing/Landing"));
+const AdminLogin = lazy(() => import("./views/Auth/Login"));
+const AdminRegister = lazy(() => import("./views/Auth/Logout"));
+const AdminProfile = lazy(() => import("./views/Dashboard/Profile/Profile"));
+const Dashboard = lazy(() => import("./views/Dashboard/Dashboard"));
+
+export const Context = createContext();
 
 function App() {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  console.log(product_data)
-// api/login 
-  // async function refreshToken(){
-  //   const refreshTok=localStorage.getItem("refresh-token");
-  //   try{
-  //     const res=await axios.post
-  //   }
-  // }
+  console.log(product_data);
+
   return (
-    <Context.Provider value={{isMenuOpen, setIsMenuOpen}}>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path="admin/login" element={<AdminLogin />} />
-        <Route path="admin/register" element={<AdminRegister />} />
-        <Route path="admin/profile" element={<AdminProfile />} />
-        <Route path='dashboard/*' element={<Dashboard />} />
-        
-      </Routes>
+    <Context.Provider value={{ isMenuOpen, setIsMenuOpen }}>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="admin/login" element={<AdminLogin />} />
+          <Route path="admin/register" element={<AdminRegister />} />
+          <Route path="admin/profile" element={<AdminProfile />} />
+          <Route path="dashboard/*" element={<Dashboard />} />
+        </Routes>
+      </Suspense>
     </Context.Provider>
   );
 }
--- src/components/Orders/Order/AllOrders.jsx
+++ src/components/Orders/Order/AllOrders.jsx
@@ -1,52 +1,55 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./Orders.css";
-import { HiChartBarSquare } from "react-icons/hi2";
-import { SiGoogledocs } from "react-icons/si";
-import { MdDiscount } from "react-icons/md";
-import { FaUser } from "react-icons/fa";
+import { MdOutlineAutoAwesomeMotion } from "react-icons/md";
+import { FaDolly } from "react-icons/fa6";
+import { AiOutlineFileDone } from "react-icons/ai";
+import { MdWatchLater } from "react-icons/md";
+import Loader from "../../../container/Loader/Loader";
 
 function AllOrders() {
   return (
-    <div className="daliy-sales display-flex">
-      <div className="daliy-sales-container">
-        <div className="daliy-sales-card order-sales-card-1">
-          <div className="daliy-sales-card-icon display-flex">
-            <HiChartBarSquare />
+    <Suspense fallback={<Loader />}>
+      <div className="daliy-sales display-flex">
+        <div className="daliy-sales-container">
+          <div className="daliy-sales-card order-sales-card-1">
+            <div className="daliy-sales-card-icon display-flex">
+              <MdOutlineAutoAwesomeMotion />
+            </div>
+            <div className="daliy-sales-card-text">
+              <h1>3</h1>
+              <span>To Do products</span>
+            </div>
           </div>
-          <div className="daliy-sales-card-text">
-            <h1>3</h1>
-            <span>To Do products</span>
+          <div className="daliy-sales-card order-sales-card-2">
+            <div className="daliy-sales-card-icon display-flex">
+              <FaDolly />
+            </div>
+            <div className="daliy-sales-card-text">
+              <h1>3</h1>
+              <span>Doing products</span>
+            </div>
           </div>
-        </div>
-        <div className="daliy-sales-card order-sales-card-2">
-          <div className="daliy-sales-card-icon display-flex">
-            <SiGoogledocs />
+          <div className="daliy-sales-card order-sales-card-3">
+            <div className="daliy-sales-card-icon display-flex">
+              <AiOutlineFileDone />
+            </div>
+            <div className="daliy-sales-card-text">
+              <h1>2</h1>
+              <span>Done products</span>
+            </div>
           </div>
-          <div className="daliy-sales-card-text">
-            <h1>3</h1>
-            <span>Doing products</span>
-          </div>
-        </div>
-        <div className="daliy-sales-card order-sales-card-3">
-          <div className="daliy-sales-card-icon display-flex">
-            <MdDiscount />
-          </div>
-          <div className="daliy-sales-card-text">
-            <h1>2</h1>
-            <span>Done products</span>
-          </div>
-        </div>
-        <div className="daliy-sales-card order-sales-card-4">
-          <div className="daliy-sales-card-icon display-flex">
-            <FaUser />
-          </div>
-          <div className="daliy-sales-card-text">
-            <h1>2</h1>
-            <span>Lated products</span>
+          <div className="daliy-sales-card order-sales-card-4">
+            <div className="daliy-sales-card-icon display-flex">
+              <MdWatchLater />
+            </div>
+            <div className="daliy-sales-card-text">
+              <h1>2</h1>
+              <span>Lated products</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
 
